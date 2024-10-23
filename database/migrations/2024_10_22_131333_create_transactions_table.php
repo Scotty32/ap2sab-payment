9bca419654ec27c22564425042d4d6a7e4d9eb23
Enoch BROU <brouenoch03@gmail.com>
2024-10-23 15:43:37 +0000

    create transaction db migration

---
+++ database/migrations/2024_10_22_131333_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->string('currency');
+            $table->string('raw_amount');
+            $table->string('transaction_uuid')->unique();
+            $table->string('payment_code')->unique();
+            $table->date('payment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
