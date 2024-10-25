563d5cb877391e8bfc90b8fc900a968a88bf47e5
eglisej.cluster030.hosting.ovh.net <eglisej-ap2sab@ssh01.cluster030.gra.hosting.ovh.net>
2024-10-25 23:25:17 +0000

    fix typo

---
+++ app/View/Composers/CountriesComposer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Composers;
+ 
+use GuzzleHttp\Client;
+use Illuminate\View\View;
+ 
+class CountriesComposer
+{
+    public function __construct(
+    ) {}
+ 
+    /**
+     * Bind data to the view.
+     */
+    public function compose(View $view): void
+    {
+        
+        $client = new Client();
+        $response = $client->get('https://restcountries.com/v3.1/all', [
+            'headers' => [
+                'Content-Type' => 'application/json'
+            ],
+        ]);
+
+        $responseData = json_decode($response->getBody(), true);
+        $countries = array_map(function($item) {
+            return $item['translations']['fra']['common'];
+        }, $responseData);
+        sort($countries);
+
+        $view->with('countries', $countries);
+    }
+}
