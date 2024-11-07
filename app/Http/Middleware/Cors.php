bd3e7118ce73b0fca8ac3f83e34e4ed6585a9387
Enoch BROU <brouenoch03@gmail.com>
2024-11-07 16:57:35 +0000

    add cors policy

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,7 +15,12 @@ class Cors
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $request->headers->set('Access-Control-Allow-Origin', '*');
-        return $next($request);
+        $response = $next($request);
+/* 
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+ */
+        return $response;
     }
 }
